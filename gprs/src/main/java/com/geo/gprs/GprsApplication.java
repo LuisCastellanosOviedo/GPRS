6e5d82645c3b763dfcdd5976e84570cf3fe669a2
Luis <luis.castellanos.oviedo@gmail.com>
2019-09-25 22:19:28 -0500

    First commit

---
+++ gprs/src/main/java/com/geo/gprs/GprsApplication.java
@@ -0,0 +1,13 @@
+package com.geo.gprs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GprsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GprsApplication.class, args);
+	}
+
+}
